20d3621a77f147909f7b70d43a323776737e4660
AlanMezva <L20170749@culiacan.tecnm.mx>
2023-11-12 16:47:21 -0700

    Practica Marvel Api terminada

--- marvelApi/JS/marvelApi.js
+++ marvelApi/JS/marvelApi.js
@@ -1,96 +1,104 @@
 var datos = [];
 var nombres = [];
-const listaUl = document.getElementById('miLista');
-listaUl.style.display = 'none';  
+const lista = document.getElementById("miLista");
+lista.style.display = "none";
+const comics = document.getElementById("comics");
+const descripcion = document.getElementById("descripcion");
+const foto = document.getElementById("foto");
 
-document.addEventListener("keyup", e => {
-    if (e.target.matches("#cantidad")) {
-        if (e.key === "Escape") e.target.value = "" 
-        listaUl.style.display = 'block';    
-        validaBusqueda();
-    }
+document.addEventListener("keyup", (e) => {
+	if (e.target.matches("#nombre")) {
+		if (e.key === "Escape") e.target.value = "";
+		lista.style.display = "block";
+		validaBusqueda();
+	}
 });
-  
+
 function validaBusqueda() {
-    console.log("entro valida");
-    const cantidad = document.getElementById("cantidad").value;
-    if (cantidad == ""){
-        listaUl.style.display = 'none';  
-        limpia();
-    }
-    else {
-        pedirDatos();
-    }
+	const nombre = document.getElementById("nombre").value;
+	if (nombre == "") {
+		lista.style.display = "none";
+		limpia();
+	} else {
+		pedirDatos();
+	}
 }
 
-/*
-document.addEventListener("keyup", e=>{
-  if (e.target.matches("#cantidad")){
-
-      if (e.key ==="Escape")e.target.value = ""
-        pedirDatos();
-  }
-})
-*/
 async function pedirDatos() {
-    const cantidad = document.getElementById("cantidad").value;
-    console.log(cantidad);
-    const respuesta = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="+cantidad);
-    datos = await respuesta.json();
-    const numNombres = Object.keys(datos.data.results).length;
-    console.log(numNombres);
-    nombres = [];
-    if (numNombres > 0) {
-        for (i=0; i < numNombres; i++){
-            nombres.push(datos.data.results[i].name);
-        }
-        muestra();
-    }
+	const nombre = document.getElementById("nombre").value;
+	const respuesta = await fetch(
+		"https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=" +
+			nombre
+	);
+	datos = await respuesta.json();
+	const numNombres = Object.keys(datos.data.results).length;
+	console.log(numNombres);
+	nombres = [];
+	if (numNombres > 0) {
+		for (i = 0; i < numNombres; i++) {
+			nombres.push(datos.data.results[i].name);
+		}
+		muestra();
+	}
 }
 
 async function selected() {
-    cantidad = document.getElementById("cantidad").value;
-    const respuesta = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="+cantidad);
-    datos = await respuesta.json();
-    foto();
+	const nombre = document.getElementById("nombre").value;
+	const respuesta = await fetch(
+		"https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=" +
+			nombre
+	);
+	datos = await respuesta.json();
+	fotito();
 }
 
 function muestra() {
-    const lista = document.getElementById("miLista");
-    const input = document.getElementById("cantidad");
+	comics.classList.add("disabled");
+	descripcion.classList.add("disabled");
+	lista.innerHTML = "";
+	descripcion.innerHTML = "";
+	comics.innerHTML = "";
 
-    lista.innerHTML = '';
+	nombres.forEach(function (nombre) {
+		const nuevoElemento = document.createElement("li");
+		const texto = document.createTextNode(nombre);
 
-    nombres.forEach(function(nombre) {
-        const nuevoElemento = document.createElement("li");
-        const texto = document.createTextNode(nombre);
+		const enlace = document.createElement("a");
+		enlace.href = "#"; // Asignar un enlace de referencia para el comportamiento predeterminado
+		enlace.appendChild(texto);
 
-        const enlace = document.createElement("a");
-        enlace.href = "#"; // Asignar un enlace de referencia para el comportamiento predeterminado
-        enlace.appendChild(texto);
-            
-        enlace.addEventListener("click", function(e) {
-          e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-           // Muestra el valor del texto del <li> al que está asociado el enlace
-           input.value = nombre;
-           lista.innerHTML = '';
-           listaUl.style.display = 'none';  
-           selected();
-        });
-        
-        nuevoElemento.appendChild(enlace);
-        lista.appendChild(nuevoElemento);
-    });
+		enlace.addEventListener("click", function (e) {
+			e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+			const input = document.getElementById("nombre");
+			input.value = nombre;
+			lista.innerHTML = "";
+			lista.style.display = "none";
+			selected();
+		});
+		nuevoElemento.appendChild(enlace);
+		lista.appendChild(nuevoElemento);
+	});
 }
 
 function limpia() {
-    const lista = document.getElementById("miLista");
-    lista.innerHTML = '';
-    let foto = document.getElementById("foto");
-    foto.setAttribute("src","images/user.png");
+	lista.innerHTML = "";
+	descripcion.innerHTML = "";
+	comics.innerHTML = "";
+	foto.setAttribute("src", "images/user.png");
 }
 
-function foto (){
-    let foto = document.getElementById("foto");
-    foto.setAttribute("src", datos.data.results[0].thumbnail.path+ "." +datos.data.results[0].thumbnail.extension);
+function fotito() {
+	comics.classList.remove("disabled");
+	descripcion.classList.remove("disabled");
+	foto.setAttribute(
+		"src",
+		datos.data.results[0].thumbnail.path +
+			"." +
+			datos.data.results[0].thumbnail.extension
+	);
+	descripcion.innerHTML = datos.data.results[0].description;
+	let top = descripcion.clientHeight;
+	comics.style.top = top + 60 + "px";
+	comics.innerHTML =
+		"Comics available: " + datos.data.results[0].comics.available;
 }
